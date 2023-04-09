cfcda39bef607c255aa2dfad95c34e8b0aa137d4
Angel Montoya <j.gustavo.m245@gmail.com>
2023-04-09 16:07:22 -0600

    add validations

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,11 +1,26 @@
 import { TaskStatus } from '../tasks.entity';
+import { IsNotEmpty, IsOptional, IsString, MinLength, IsIn } from 'class-validator';
 
 export class createTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(4)
     title: string;
+
+    @IsString()
     description: string;
 }
 export class updateTaskDto {
+    @IsString()
+    @IsOptional()
     title?: string;
+
+    @IsString()
+    @IsOptional()
     description?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
     status?: TaskStatus;
 }
